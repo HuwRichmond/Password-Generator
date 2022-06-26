e0b8ca833a3f52a78fc635674cd965756b9c198e
unknown <git@internode.on.net>
2022-06-27 08:32:47 +0930

    First commit of password generator to github

---
+++ script.js
@@ -0,0 +1,27 @@
+// Password generator code - currently not working as intended.
+var generateBtn = document.querySelector("#generate");
+
+var password=document.getElementById("password");
+
+ function writePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
