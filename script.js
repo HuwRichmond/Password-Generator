57a1a761dc7c1f0443266bff4b24052599e1de9a
unknown <git@internode.on.net>
2022-06-27 13:57:10 +0930

    JSS restarted from scratch, still not working

--- script.js
+++ script.js
@@ -1,24 +1,103 @@
-// Password generator code - currently not working as intended.
-//variables assigned to the 2 ID tags from the HTML - the ID's are generate and password
+// Special characters for the function created
+const specialCharacters = "!@#$%^&*()";
+const generateButton = document.getElementById('generateBtn')
+generateButton.addEventListener('click' = writePassword)
 
 // Write password to the #password input
-var generateBtn = document.querySelector("#generate");
-
-var password=document.getElementById("#password");
-
 function writePassword() {
-  var password = generatePassword("");
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-for (var i = 0; i <= passwordLength; i++) {
- var randomNumber = Math.floor(Math.random() * chars.length);
- password += chars.substring(randomNumber, randomNumber +1);
-}
-      document.getElementById("#password").value = password;
+  const specialCharacters = "!@#$%^&*()";
+  const generateButton = document.getElementById('generateBtn')
+  generateButton.addEventListener('click' = writePassword)
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Prompts that come up after you click generate password
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowerCases = confirm("Do you want lowercases in your password?");
+  var upperCases = confirm("Do you want uppercases in your password?");
+  var special = confirm("Do you want special characters in your password?");
+
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 1;
+
+
+  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
+
+  var minimumNumbers = 1;
+  var minimumLowerCases = 1;
+  var minimumUpperCases = 1;
+  var minimumSpecialCharacters = 1;
+
+
+  // Generator functions**
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+
+};
+
+  // Checks to make sure user selected ok for all and uses empty minimums from above
+
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+
+  }
+
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCount++;
+
+  }
+
+  // empty string variable for the for loop below
+  var randomPasswordGenerated = "";
+
+  // loop getting random characters
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+
+    randomPasswordGenerated += randomNumberPicked;
+
+  }
+
+  // to make sure characters are added to the password
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+
+
+  return randomPasswordGenerated;
+
+}
