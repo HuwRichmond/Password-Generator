f498660e5acc9facf1cba9752f8f0d46515a8d59
unknown <git@internode.on.net>
2022-06-27 11:14:13 +0930

    password still not displaying correctly

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 // Password generator code - currently not working as intended.
-var generateBtn = document.querySelector("#generate");
-
-var password=document.getElementById("password");
-
- function writePassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 12;
-    var password = "";
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  }
-        document.getElementById("password").value = password;
- }
+//variables assigned to the 2 ID tags from the HTML - the ID's are generate and password
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
 
+var password=document.getElementById("#password");
+
+function writePassword() {
+  var password = generatePassword("");
+  var passwordText = document.querySelector("#password");
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12;
+for (var i = 0; i <= passwordLength; i++) {
+ var randomNumber = Math.floor(Math.random() * chars.length);
+ password += chars.substring(randomNumber, randomNumber +1);
+}
+      document.getElementById("#password").value = password;
   passwordText.value = password;
 
 }
