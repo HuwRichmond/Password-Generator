1a9ddefe8d38ebaa4de1a8288866d36d96930dde
unknown <git@internode.on.net>
2022-06-27 14:22:13 +0930

    comments added and Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Password-Generator
 Random password generator
+
+Starting HTML and CSS code used for the random password generator made with javascript.
+
+The password generator is intended to generate a 12 character password when the generate button is clicked and the randomly generated password will appear in the text box so it can be copied and used.
+
+Generator to have the following features:
+- generates a password when the generate button is clicked
+- presents users with prompts to select their password length (between 8 and 128 characters)
+- prompts users if they want uppercase, lowercase, nembers and special characters in the password.
+- generates a passwork in accordance with criteria selected through the prompts.
+
+Comments added on the JS file describing individual lines on the JS code
+
+
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Special characters for the function created
+// special characters specified here
 const specialCharacters = "!@#$%^&*()";
 const generateButton = document.getElementById('generateBtn')
+//chrome devtools indicates there is an error with this line of code.
 generateButton.addEventListener('click' = writePassword)
 
-// Write password to the #password input
+// here i have assigned the variables generate password and the writing of the password text to the write password function.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -15,6 +16,7 @@ function writePassword() {
 }
 
 // Prompts that come up after you click generate password
+//each prompt has been assigned a variable under the "generate password" function so make the prompts appear after the generate button is used.
 function generatePassword() {
   var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
   var numbers = confirm("Do you want numbers in your password?");
@@ -22,32 +24,33 @@ function generatePassword() {
   var upperCases = confirm("Do you want uppercases in your password?");
   var special = confirm("Do you want special characters in your password?");
 
-  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
-  var minimumCount = 1;
+  // this sets the minimum password length - assessment criteria specific passwords must be between 8 and 128 characters.
+  var minimumCount = 8;
 
 
-  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
+  // no values (empty values) have been assigned to the properties as users are supposed to be able to select if they want numbers, special characters etc.
 
-  var minimumNumbers = 1;
-  var minimumLowerCases = 1;
-  var minimumUpperCases = 1;
-  var minimumSpecialCharacters = 1;
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
 
 
-  // Generator functions**
+  // Generator functions have been grouped together in an array called "functionArray" it is intended to take commands from the prompt boxes to then use the .math.random command to gereate the password.
   var functionArray = {
+    //this is the numbers for the password
     getNumbers: function() {
       return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
     },
-
+//lowercase characters variable
     getLowerCases: function() {
       return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
     },
-
+//uppercase variable
     getUpperCases: function() {
       return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
     },
-
+//special characters variable - the "specialCharacters variable was specified above with an array of "!@#$%^&*()"
     getSpecialCharacters: function() {
       return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
     }
